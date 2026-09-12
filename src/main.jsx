f624a8eb0e083304f7e04276db9cf8a43a1d9fc0
Avinash Kumar <178039057+Avinashkr000@users.noreply.github.com>
2026-09-12 16:38:38 +0530

    feat: apply marketplace design layer

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './styles.css'
 import './app.css'
+import './marketplace.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
